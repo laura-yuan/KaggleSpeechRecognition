c298db2173e3237b5af222036cb6528bcf182123
Juyue_LYPC <jychenpek@gmail.com>
2017-12-25 01:04:21 -0500

    some batch file for myself.

---
+++ tensorflow_official/speech_commands/run_freezepy_conv_deep.bat
@@ -0,0 +1,1 @@
+python freeze.py --data_url= --start_checkpoint=E:/Juyue/tmp_conv_deep_norm/speech_commands_train/conv_deep.ckpt-16000 --output_file=E:/Juyue/tmp_conv_deep_norm/my_frozen_graph_conv.pb --model_architecture=conv_deep
